29edc03553085efba49383d5d4580e6a4e324feb
NengtaiChiu <78375749+NengtaiChiu@users.noreply.github.com>
2023-06-28 16:40:16 +0800

    Add files via upload

---
+++ PVP_SQI.m
@@ -0,0 +1,65 @@
+function [SQI] = PVP_SQI(PVP_dtr,sampling_rate)
+%PVP_SQI Summary of this function goes here
+%Input:
+% 'PVP_dtr': the detrend PVP signal.
+% 'sampling_rate': the sampling rate of the PVP signal.
+%Output:
+% 'SQI': the proposed SQI index for each ten second epoch
+addpath('TF_anaylsis');
+
+basicTF.win = sampling_rate*10+1;% window length of SST
+basicTF.hop = sampling_rate/20;%window hopping frequency
+basicTF.fs = sampling_rate ;
+basicTF.fr = 0.01; % frequency resolution
+% advanced parameters for STFT
+advTF.HighFreq = 10/basicTF.fs; % highest frequency/sampling freq
+advTF.LowFreq = 0.01/basicTF.fs;
+%% Syncro Squeezing Transform of the PVP signal
+[~, ~, PVP_tfrsq, ~, tfrsqtic] = ConceFT_sqSTFT_C(PVP_dtr, 0, advTF.HighFreq, basicTF.fr/basicTF.fs,basicTF.hop, basicTF.win, 1, 6, 1, 1, 0) ;
+%% extract the first three harmonics of the PVP signal
+tic;
+[harmonics] = multi_curve_extract(PVP_tfrsq, tfrsqtic, sampling_rate, 10, 3,10);
+toc;
+%% reconstruct cardiac signal;
+card_tfrsq = zeros(size(PVP_tfrsq)); % the tfr used for reconstruction
+
+band_Hz = 0.2/2;% bandwith of cardiac component in Hz.
+
+for l = 1:3
+    for k = 1:size(PVP_tfrsq,2)
+        card_tfrsq(harmonics(l,k)-band_Hz/basicTF.fr+1:harmonics(l,k)+band_Hz/basicTF.fr,k) = PVP_tfrsq(harmonics(l,k)-band_Hz/basicTF.fr+1:harmonics(l,k)+band_Hz/basicTF.fr,k);
+    end
+end
+
+alpha = tfrsqtic(2)-tfrsqtic(1);
+[h, ~] = hermf(sampling_rate*10+1, 1, 6) ;
+coeff = h(sampling_rate*5+1) ;%
+C = 2 * alpha / coeff ; % the normalizing coeffient for the reconstruction of SST
+
+recon = zeros(1,size(card_tfrsq,2));% reconstruct cardiac signal with sampling rate same as the hopping frequency = basicTF.hop
+for kk = 1: size(card_tfrsq,2)
+    recon(kk) = C * sum(card_tfrsq(:,kk),1) ;
+end
+% resample the reconstructed cardiac signal into the samme sampling rate as
+% the PVP signal.
+recon_cardiac = interp1([1:basicTF.hop:length(PVP_dtr)]/sampling_rate,real(recon),[1:1:length(PVP_dtr)]/sampling_rate,'pchip')';
+%% Get SQI
+PVP_noise = PVP_dtr - recon_cardiac;
+
+SQI_tmp = zeros(size(PVP_tfrsq,2),1);% the SQI index with sampling rate = hopping frequency.
+for k = 1:size(PVP_tfrsq,2)-1
+    cardiac_power = zeros(1,3);
+    for l = 1:3
+        cardiac_power(l)= sum(PVP_tfrsq(harmonics(l,k)-band_Hz/basicTF.fr+1:harmonics(l,k)+band_Hz/basicTF.fr,k));
+    end
+    SQI_tmp(k) = sum((abs(C*cardiac_power)).^2)/(sum((abs(C*cardiac_power)).^2)+sum(PVP_noise((k-1)*basicTF.hop+1:k*basicTF.hop).^2)/basicTF.hop);
+end
+epoch_len = 10;% time length of PVP epochs, unit=second.
+epoch_num = floor(length(PVP_dtr)./(sampling_rate*epoch_len));
+SQI = zeros(epoch_num,1);
+for i = 1:epoch_num
+    s = (i-1)*sampling_rate/basicTF.hop*10;
+    t = i*sampling_rate/basicTF.hop*10;
+    SQI(i)= median(SQI_tmp(s+1:t));
+end
+
---
+++ PVP_demo.m
@@ -0,0 +1,40 @@
+clear all;close all;
+%% generate the simulated data
+fs = 100;% sampling_rate
+time = [1/fs:1/fs:120]' ;
+N = length(time) ;
+%% the instantaneous frequency of the simulated signal
+if1 = smooth(cumsum(randn(N,1)) ./ fs, 400, 'loess') ;
+if1 = 1.2 + 0.2 * if1 ./ max(abs(if1)) ;
+if2 = 2.4 + 0.2 * if1 ./ max(abs(if1)) ;
+if3 = 0.2 + 0.05 * if1 ./ max(abs(if1)) ;
+phi1 = cumsum(if1) / fs ; 
+phi2 = cumsum(if2) / fs ; 
+phi3 = cumsum(if3) / fs ; 
+
+%% the simulated signal.
+trend = smooth(cumsum(randn(N,1))+10, 1000, 'loess') ;
+s1 = 1* cos(2*pi*phi1) ; 
+s2 = 0.4* cos(2*pi*phi1) ; 
+s3 = 3 * cos(2*pi*phi1) ; 
+clean = s1+s2+s3+trend;
+
+%% add noise (Gaussian white noise)
+sigma = 0.5;
+noise = random('T',4,N,1) ;
+noise = sigma * noise ; 
+%% simulated observed time series
+phantom_PVP = clean + noise ;
+%% Preprocess signal
+[b, a] = butter(6, [0.5, 15] / (100 / 2));% bandpass filter setting.
+y = median_filter(phantom_PVP, 6*fs); % trend
+y(1) = y(2);
+y = smooth(y, 12*fs, 'loess'); % smooth trend
+PVP_dtr = filtfilt(b, a, phantom_PVP-y);% bandpass filter
+%% Get SQI index
+[SQI] = PVP_SQI(PVP_dtr,fs);
+
+
+
+
+
